7070a461a67f0539971f9e6926a621c009c5edff
Romain Andres <romain.andres@etu.unicaen.fr>
2023-12-26 15:43:15 +0100

    deletion of all useless permissions, big optimisation of the code

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'activateFunction') {
-        // Envoie un message au contenu du script pour activer la fonction
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'activateFunction' });
-        });
-    }
-    // Ajoutez le traitement pour d'autres actions si nécessaire
-});
--- contentScript.js
+++ contentScript.js
@@ -5,22 +5,10 @@ function countTokens(str) {
 let tokenCounterDiv = null;
 // recuperer le texte du prompt chatgpt:
 let inputField = document.querySelector('#prompt-textarea');
-let isHidden = false;
-let isActivedByDefault = true;
-
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.action === 'activateFunction') {
-        // Appelez la fonction à activer ici
-        hideAndActivate();
-    }
-    // Ajoutez le traitement pour d'autres actions si nécessaire
-});
 
 function setupTokenCounter() {
-    /*
-    if (tokenCounterConfigured) return;
-    tokenCounterConfigured = true;
-    */
+    inputField = document.querySelector('#prompt-textarea');
+
     if (inputField) {
         tokenCounterDiv = document.createElement('div');
         //rendre le truc flotant pour le foutre au dessus:
@@ -34,14 +22,12 @@ function setupTokenCounter() {
         tokenCounterDiv.style.color = 'white';
         tokenCounterDiv.textContent = 'Tokens: 0';
         tokenCounterDiv.style.zIndex = '10000';
-        tokenCounterDiv.style.textContent = "Tokens: 0"
+        //tokenCounterDiv.style.textContent = "Tokens: 0"                     A DELETE
 
         setTimeout(() => {
             inputField.insertAdjacentElement('beforebegin', tokenCounterDiv);
         }, 1000);
-        if (isActivedByDefault) {
-            inputField.addEventListener('input', updateTokenCount);
-        }
+        inputField.addEventListener('input', updateTokenCount);
     }
 }
 
@@ -52,45 +38,19 @@ function removeTokenCounter() {
     }
 }
 
-function createTokenCounter() {
-    inputField = document.querySelector('#prompt-textarea');
-    setupTokenCounter();
-
-}
-
 function updateTokenCount() {
-    if (!isHidden && isActivedByDefault) {
-        tokenCounterDiv.textContent = 'Tokens: ' + countTokens(inputField.value);
-    }
+    tokenCounterDiv.textContent = 'Tokens: ' + countTokens(inputField.value);
 }
-
-setupTokenCounter();
-
-function hideAndActivate() {
-    isHidden = !isHidden;
-    isActivedByDefault = !isActivedByDefault;
-
-
-    if (isActivedByDefault) {
-        inputField.addEventListener('input', updateTokenCount);
-        tokenCounterDiv.style.display = 'block'; // Afficher le compteur de jetons
-    } else {
-        inputField.removeEventListener('input', updateTokenCount);
-        tokenCounterDiv.style.display = 'none'; // Masquer le compteur de jetons
-    }
-}
-
 
 // Utiliser un observeur pour détecter les changements dans l'URL au lieu d'un setInterval
 let lastUrl = location.href; 
 new MutationObserver(() => {
     const url = location.href;
     if (url !== lastUrl) {
-        console.log("CHANGEMENT !!!")
         lastUrl = url;
         removeTokenCounter();
-        tokenCounterConfigured = false; // Reset le flag
         setupTokenCounter();
-        createTokenCounter();
     }
 }).observe(document, { subtree: true, childList: true });
+
+setupTokenCounter();
